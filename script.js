fbdddc48278af73d38ef1ffb024c181f4c0d1f63
suinaliev08 <82721180+suinaliev08@users.noreply.github.com>
2021-09-02 14:52:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const Hmenu = document.querySelector('.m-icon');
+let close = document.querySelector('.close i');
+let visible = document.querySelector('.links-menu');
+let moon = document.querySelector('.moon');
+
+Hmenu.addEventListener('click', () => {
+    visible.classList.add('visible');
+    document.querySelector('.links-menu-bg').style.display='block';
+});
+
+close.addEventListener('click', () => {
+    visible.classList.remove('visible');
+    document.querySelector('.links-menu-bg').style.display='none';
+});
+
+moon.onclick = function (event) {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.querySelector('.moon i').classList.add('bx-moon');
+        document.querySelector('.moon i').classList.remove('bx-sun');
+        document.querySelector('.ch').innerHTML = 'Dark Theme';
+    } else {
+        document.body.classList.add('dark');
+        document.querySelector('.moon i').classList.remove('bx-moon');
+        document.querySelector('.moon i').classList.add('bx-sun');
+        document.querySelector('.ch').innerHTML = 'Light Theme';
+    }
+};
